6346d741de5e62b0533a6c70ae54318d72e6e2d5
jimbowler82 <j.bowler2017@gmail.com>
2023-11-19 18:55:32 +0000

    Add comments entity and handlers

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -3,7 +3,7 @@
     public class Activity
     {
         public Guid Id { get; set; }
-        public string  Title { get; set; }
+        public string Title { get; set; }
         public DateTime Date { get; set; }
         public string Description { get; set; }
         public string Category { get; set; }
@@ -11,6 +11,7 @@
         public string Venue { get; set; }
         public bool IsCancelled { get; set; }
         public ICollection<ActivityAttendee> Attendees { get; set; } = new List<ActivityAttendee>();
+        public ICollection<Comment> Comments { get; set; } = new List<Comment>();
 
     }
 }
