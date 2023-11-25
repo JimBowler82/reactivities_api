a71b32ce93f4916155714ea554f13102e9ac2c96
jimbowler82 <j.bowler2017@gmail.com>
2023-11-25 12:03:19 +0000

    Add following property, add pagination and filtering

---
+++ API/Extensions/HttpExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+
+namespace API.Extensions
+{
+    public static class HttpExtensions
+    {
+        public static void AddPaginationHeader(this HttpResponse response, int currentPage, int itemsPerPage, int totalItems, int totalPages)
+        {
+            // Create a new instance of PaginationHeader
+            var paginationHeader = new { currentPage, itemsPerPage, totalItems, totalPages };
+
+            // Create a new instance of JsonSerializerOptions
+            var options = new JsonSerializerOptions
+            {
+                // Set the property name case to camelCase
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+            // Serialize the paginationHeader object to a JSON string
+            // and store it in the paginationHeaderJson variable
+            var paginationHeaderJson = JsonSerializer.Serialize(paginationHeader, options);
+
+            // Add the paginationHeaderJson string to the response header
+            response.Headers.Add("Pagination", paginationHeaderJson);
+
+            // Add the Access-Control-Expose-Headers header to the response
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+        }
+    }
+}
